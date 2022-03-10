b71b3723c309fac1b4a05a0815aeed2e84332ba0
Felipe Fazani <lipefazani@gmail.com>
2022-03-10 19:29:38 -0300

    Adicionando o Download
    A função do download é baixar todos os arquivos .csv de cada região

--- Programa/WebScraping.py
+++ Programa/WebScraping.py
@@ -1,7 +1,10 @@
+import os
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class WebScraping:
@@ -9,8 +12,11 @@ class WebScraping:
         self.driver = webdriver.Chrome()
         self.driver.get(url)
 
-    def altera_regioes(self, lista_regioes):
-        
-
-
-
+    def download(self, lista_regioes):
+        elem = self.driver.find_element(By.ID, "conteudo_btnMensal")
+        elem.click()
+        for regiao in lista_regioes:
+            WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "conteudo_ddlRegioes")))
+            select = Select(self.driver.find_element(By.ID, "conteudo_ddlRegioes"))
+            select.select_by_visible_text(regiao)
+            self.driver.find_element(By.ID, "conteudo_btnExcel").click()
--- Programa/main.py
+++ Programa/main.py
@@ -1,5 +1,10 @@
+import WebScraping as Wb
 
 if __name__ == '__main__':
-    pass
+    obj = Wb.WebScraping("http://www.ssp.sp.gov.br/Estatistica/Pesquisa.aspx")
+    regioes = ["Capital", "Grande São Paulo (exclui a Capital)", "São José dos Campos", "Campinas", "Ribeirão Preto",
+               "Bauru", "São José do Rio Preto", "Santos", "Sorocaba", "Presidente Prudente", "Piracicaba", "Araçatuba"]
+
+    obj.download(regioes)
 
 
