aa6b989c01db4795a9f998e7dd13f169ccbc0bc8
Felipe Fazani <lipefazani@gmail.com>
2022-03-11 19:00:32 -0300

    Finalizando o método Organiza
    o método organiza junta todos arquivos baixados em uma única tabela

--- Programa/WebScraping.py
+++ Programa/WebScraping.py
@@ -16,33 +16,50 @@ class WebScraping:
         self.caminho_downloads = caminho_downloads
 
     def download(self):
-        ''' Baixa todos os csv separados por região '''
+        """ Baixa todos os csv separados por região """
         elem = self.driver.find_element(By.ID, "conteudo_btnMensal")
         elem.click()
         for regiao in self.lista_regioes:
-            baixados = [baixado for baixado in os.listdir(self.caminho_downloads) if regiao+".csv" in baixado]
-            if "Mensal-Região "+regiao+".csv" not in baixados:
+            baixados = [baixado for baixado in os.listdir(self.caminho_downloads) if regiao + ".csv" in baixado]
+            if "Mensal-Região " + regiao + ".csv" not in baixados:  # apenas baixa aqueles que ainda não foram baixados
                 WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "conteudo_ddlRegioes")))
                 select = Select(self.driver.find_element(By.ID, "conteudo_ddlRegioes"))
                 select.select_by_visible_text(regiao)
                 self.driver.find_element(By.ID, "conteudo_btnExcel").click()
 
-
     def organiza(self):
+        """ Organiza todos os arquivos baixados em uma única tabela """
+        matriz_organizada = []
         for arquivo in os.listdir(self.caminho_downloads):
             for regiao in self.lista_regioes:
-                if regiao+".csv" in arquivo:
-                    print("Arquivo: ", arquivo, "   Regiao: ", regiao)
+                if regiao + ".csv" in arquivo:  # verifica se o arquivo dentro da pasta download é uma das regiões
                     with open(self.caminho_downloads + "/" + arquivo, "r", newline='\n') as arq:
                         leitor = csv.reader((x.replace('\0', '') for x in arq), delimiter=';', quotechar='|')
-                        with open("Dataset/dataframe.csv", "a") as df:
-                            escritor = csv.writer(df)
-                            for linha in leitor:
-                                if len(linha) != 0:
-                                    if linha[0] == '2021' or linha[0] == '2020':
-                                        ano = int(linha[0])
-                                    elif linha[0] == 'Ocorrencia':
-                                        continue
-                                    else:
-                                        # linha[0] vai ser o 
+                        for linha in leitor:
+                            if len(linha) != 0:
+                                if linha[0] == '2021' or linha[0] == '2020':
+                                    ano = int(linha[0])
+                                    pode = True
+                                elif linha[0] == '2022':
+                                    pode = False
+                                elif linha[0] == 'Ocorrencia' or len(linha) <= 1:
+                                    continue
+                                elif pode:
+                                    meses = {1: "Jan", 2: "Fev", 3: "Mar", 4: "Abr", 5: "Mai", 6: "Jun",
+                                             7: "Jul", 8: "Ago", 9: "Set", 10: "Out", 11: "Nov", 12: "Dez"}
+                                    natureza = linha[0]
+                                    for i in range(1, 13):
+                                        linha_organizada = []
+                                        ocorencia_mes = linha[i]
+                                        linha_organizada.append(natureza)
+                                        linha_organizada.append(meses[i])
+                                        linha_organizada.append(ano)
+                                        linha_organizada.append(regiao)
+                                        linha_organizada.append(ocorencia_mes)
+                                        matriz_organizada.append(linha_organizada)
 
+        colunas = ["Natureza", "Mes", "Ano", "Regiao", "Ocorrencias"]
+        df = pd.DataFrame(matriz_organizada, columns=colunas)
+        df.to_csv("dataframe.csv", index=False)
+        df = pd.read_csv("dataframe.csv")
+        print(df)
--- Programa/main.py
+++ Programa/main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     url = "http://www.ssp.sp.gov.br/Estatistica/Pesquisa.aspx"
     regioes = ["Capital", "Grande São Paulo (exclui a Capital)", "São José dos Campos", "Campinas", "Ribeirão Preto",
                "Bauru", "São José do Rio Preto", "Santos", "Sorocaba", "Presidente Prudente", "Piracicaba", "Araçatuba"]
-    caminho = "C:/Users/fatecano/Downloads"
+    caminho = "C:/Users/Felipe/Downloads"
 
     obj = Wb.WebScraping(url, regioes, caminho)
     obj.download()
