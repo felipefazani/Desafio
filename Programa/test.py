f40e995aabc34a2af0ce446ffd9a7325c50c4ca1
Felipe Fazani <lipefazani@gmail.com>
2022-03-30 18:33:32 -0300

    Criando TestaMain
    TestaMain class was created to test the main.py

---
+++ Programa/test.py
@@ -0,0 +1,20 @@
+import unittest
+import main
+
+
+class TestaMain(unittest.TestCase):
+
+    def test_maiores(self):
+        data = {"a": 249, "b": 260, "c": 200, "d": 230}
+        result = main.maiores(data, 2)
+        self.assertEqual(result[0][1], 260, "Maiores deveria uma lista organizada com os maiores")
+
+    def test_maiores_n(self):
+        data = {"a": 249, "b": 260, "c": 200, "d": 230}
+        result = main.maiores(data, 2)
+        self.assertEqual(len(result), 2, "Maiores deveria retornar uma lista com tamanho n")
+
+    def test_maiores_tupla(self):
+        data = {"a": 249, "b": 260, "c": 200, "d": 230}
+        result = main.maiores(data, 2)
+        self.assertEqual(type(result[0]), type(()), "Maiores deveria retornar uma lista de tuplas")
