a1f1454c2f2958345c9fe01313903f30f9c4b9e3
Felipe Fazani <lipefazani@gmail.com>
2022-03-31 16:52:34 -0300

    Update test.py
    Adicionando setUp para evitar redundancia no codigo

--- Programa/test.py
+++ Programa/test.py
@@ -4,17 +4,15 @@ import main
 
 class TestaMain(unittest.TestCase):
 
+    def setUp(self):
+        self.data = {"a": 249, "b": 260, "c": 200, "d": 230}
+        self.result = main.maiores(self.data, 2)
+
     def test_maiores(self):
-        data = {"a": 249, "b": 260, "c": 200, "d": 230}
-        result = main.maiores(data, 2)
-        self.assertEqual(result[0][1], 260, "Maiores deveria uma lista organizada com os maiores")
+        self.assertEqual(self.result[0][1], 260, "Maiores deveria uma lista organizada com os maiores")
 
     def test_maiores_n(self):
-        data = {"a": 249, "b": 260, "c": 200, "d": 230}
-        result = main.maiores(data, 2)
-        self.assertEqual(len(result), 2, "Maiores deveria retornar uma lista com tamanho n")
+        self.assertEqual(len(self.result), 2, "Maiores deveria retornar uma lista com tamanho n")
 
     def test_maiores_tupla(self):
-        data = {"a": 249, "b": 260, "c": 200, "d": 230}
-        result = main.maiores(data, 2)
-        self.assertEqual(type(result[0]), type(()), "Maiores deveria retornar uma lista de tuplas")
+        self.assertEqual(type(self.result[0]), type(()), "Maiores deveria retornar uma lista de tuplas")
